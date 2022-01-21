b134f205bc89162203368c18f534c6f45d31e3b8
Alex <silius@gmail.com>
2022-01-21 11:39:17 +0200

    Home exercises 21

--- js/learn_21_Practic.js
+++ js/learn_21_Practic.js
@@ -65,11 +65,31 @@ const personalMovieDB = {
     }
   },
   writeYourGenres: function () {
-    for (let i = 1; i <= 3; i++) {
-      personalMovieDB.genres[i - 1] = prompt(
-        `Ваш любимый жанр под номером ${i}`
-      );
+    for (let i = 1; i < 2; i++) {
+      // for (let i = 1; i <= 3; i++) {
+      // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+      // if (genre === "" || genre == null) {
+      //   console.log("Вы ввели некорректные данные или не ввели их вовсе");
+      //   i--;
+      // } else {
+      //   personalMovieDB.genres[i - 1] = genre;
+      // }
+      let genres = prompt(
+        `Введите ваши любимые жанры через заяпятую`
+      ).toLocaleLowerCase();
+
+      if (genres === "" || genres == null) {
+        console.log("Вы ввели некорректные данные или не ввели их вовсе");
+        i--;
+      } else {
+        personalMovieDB.genres = genres.split(", ");
+        personalMovieDB.genres.sort();
+      }
     }
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
   },
 };
 personalMovieDB.start();
